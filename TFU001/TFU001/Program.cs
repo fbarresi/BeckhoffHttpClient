289219e1cce6e7fe2d59f9e0864d566aceef7b34
Federico Barresi <federico.barresi@evopro-ag.de>
2019-07-04 16:31:50 +0200

    Added library implementation

--- TFU001/TFU001/Program.cs
+++ TFU001/TFU001/Program.cs
@@ -4,6 +4,7 @@ using McMaster.Extensions.CommandLineUtils;
 using Newtonsoft.Json;
 using RestSharp;
 using RestSharp.Serialization.Json;
+using TwinCAT.Ads;
 
 namespace TFU001
 {
@@ -14,22 +15,32 @@ namespace TFU001
 
         [Argument(0, "Address")]
         public string Address { get; set; } = "https://dog.ceo/api/breeds/image/random";
-        [Argument(1, "Call Method")]public Method Method { get; set; }
-        [Argument(2, "Headers")] public string Headers { get; set; }
-        [Argument(3, "Parameters")] public string Parameters { get; set; }
+        [Argument(1, "Method")] public Method Method { get; set; }
+        [Argument(3, "ResponseCode")] public string ResponseCode { get; set; }
+        [Argument(4, "Body")] public string Body { get; set; }
+        [Argument(5, "Response")] public string Response { get; set; }
+
+        [Option(ShortName = "AdsNetId")]public string AdsNetId { get; set; } = "";
+        [Option(ShortName = "AdsPort")]public int AdsPort { get; set; } = 851;
 
 
         public void OnExecute()
         {
-            var client = new RestClient();
+            Console.WriteLine($"ResponseCode : {ResponseCode}");
+            var restClient = new RestClient();
             var request = new RestRequest(Address, Method);
             //request.AddParameter("name", "value");
             //request.AddHeader("header", "value");
-            var response = client.Execute(request);
+            var response = restClient.Execute(request);
             Console.WriteLine(response.Content);
-
+            var adsClient = new TcAdsClient { Synchronize = false, };
+            adsClient.Connect(AdsNetId, AdsPort);
+            var responseCodeHandle = adsClient.CreateVariableHandle(ResponseCode);
+            adsClient.WriteAny(responseCodeHandle, (short)response.StatusCode);
+            adsClient.Disconnect();
+            adsClient.Dispose();
             var jsonObject = JsonConvert.DeserializeObject(response.Content);
-            Console.WriteLine($"{response.StatusCode} - {(int)response.StatusCode}" );
+            Console.WriteLine($"{response.StatusCode} - {(short)response.StatusCode}" );
         }
     }
 }
